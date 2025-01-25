a73ca365e0060dbfe179e266bdbb5a0ea1a169a3
Ben Jones <ben.jones@uta.edu>
2025-01-25 06:41:16 -0600

    add gases

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     ],
     zip_safe=False,
     name='PyGasMix',  # Required
-    packages=['PyGasMix'],
+    packages=['PyGasMix','PyGasMix.Gases'],
 
     version='1.2.0',  # Required
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
